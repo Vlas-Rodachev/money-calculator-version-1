3207cbfd0da9366a837839978b4880ecbd68dda8
влас родачев <roda4evvlas@mail.ru>
2025-04-07 22:24:23 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_2
+{
+    public class IntInputValidator
+    {
+        public static int GetValidIntInput()
+        {
+            int result;
+            while (true)
+            {
+                Console.Write("Введите значение: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Неверное значение. Попробуйте снова.");
+                }
+            }
+        }
+    }
+
+    public class Money
+    {
+        private uint rubles;
+        private byte kopeks;
+
+        public Money()
+        {
+            rubles = 0; 
+            kopeks = 0; 
+        }
+
+        public Money(uint x, byte y)
+        {
+            rubles = x;
+            kopeks = y;
+            checking_kopecks();
+        }
+
+        // вычитание двух объектов
+        public static Money operator -(Money a, Money b)
+        {
+            uint rublesn;
+            byte kopeksn;
+            if (a.kopeks >= b.kopeks)
+            {
+                kopeksn = (byte)(a.kopeks - b.kopeks);
+                if (a.rubles >= b.rubles)
+                {
+                    rublesn = a.rubles - b.rubles;
+                }
+                else
+                {
+                    rublesn = 0;
+                }
+            }
+            else
+            {
+                kopeksn = (byte)(100 - (b.kopeks - a.kopeks));
+
+                if (a.rubles + 1 >= b.rubles)
+                {
+                    rublesn = a.rubles - b.rubles - 1;
+                }
+                else
+                {
+                    rublesn = 0;
+                }
+            }
+
+            return new Money(rublesn, kopeksn);
+        }
+
+        //Унарные операции ++
+        public static Money operator ++(Money x)
+        {
+            x.kopeks++;
+            x.checking_kopecks();
+            return x;   
+        }
+
+        //Унарные операции --
+        public static Money operator --(Money x)
+        {
+            if (x.kopeks > 0) x.kopeks--;
+            return x;
+        }
+
+        public void checking_kopecks()
+        {
+            rubles += (uint)(kopeks / 100);
+            kopeks = (byte)(kopeks % 100);
+        }
+
+        //Операции приведения типа
+        public static implicit operator uint(Money money) => money.rubles; 
+        public static explicit operator double(Money money) => money.kopeks / 100.0;
+
+        public static Money operator -(Money money, uint rublesToSubtract)
+        {
+            if (rublesToSubtract > money.rubles)
+                return new Money(0, 0); // Нельзя уйти в минус
+
+            return new Money(money.rubles - rublesToSubtract, money.kopeks);
+        }
+
+        public static Money operator -(uint rublesToSubtract, Money money)
+        {
+            ulong totalMoneyKopeks = (ulong)money.rubles * 100 + money.kopeks;
+            ulong totalSubtractKopeks = (ulong)rublesToSubtract * 100;
+
+            if (totalSubtractKopeks < totalMoneyKopeks)
+                return new Money(0, 0); // Нельзя получить отрицательный результат
+
+            ulong resultKopeks = totalSubtractKopeks - totalMoneyKopeks;
+            uint resultRubles = (uint)(resultKopeks / 100);
+            byte resultKopeks1 = (byte)(resultKopeks % 100);
+
+            return new Money(resultRubles, resultKopeks1);
+        }
+
+        public override string ToString()
+        {
+            return $"RUB='{rubles}', KOP='{kopeks}'";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using lab1_2;
+using System;
+
+/*Money money = new Money(123, 96);
+Money money2 = new Money(169, 69);
+Money money3 = money2 - money;
+Console.WriteLine(money3.ToString());
+money3++;
+Console.WriteLine(money3.ToString());
+uint x = money;
+double y = (double)money;
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+uint z = money - 10;
+Console.WriteLine(z);*/
+
+
+Money fu1()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите номер задания для класса Money");
+        Console.WriteLine("1 - Ввести вручную");
+        Console.WriteLine("2 - Оставить базовые значения");
+        int number_problem1 = IntInputValidator.GetValidIntInput();
+        if (number_problem1 == 1)
+        {
+            Money money = new Money((uint)(IntInputValidator.GetValidIntInput()), (byte)(IntInputValidator.GetValidIntInput()));
+            Console.WriteLine(money.ToString());
+            Console.WriteLine();
+            return money;
+        }
+        else if (number_problem1 == 2)
+        {
+            Money money = new Money();
+            Console.WriteLine(money.ToString());
+            Console.WriteLine();
+            return money;
+        }
+    }
+}
+
+Console.WriteLine("Создание первого объекта");
+Money money1 = fu1();
+Console.WriteLine("Создание второго объекта");
+Money money2 = fu1();
+bool tr1 = true;
+while (tr1)
+{
+    Console.WriteLine("Введите номер задания");
+    Console.WriteLine("1 - вычесть из первого второй объект");
+    Console.WriteLine("2 - прибавить копейку к первому объекту");
+    Console.WriteLine("3 - вычесть копейку к первому объекту");
+    Console.WriteLine("4 - получить рубли (копейки отбрасываются)");
+    Console.WriteLine("5 - получить копейки (рубли отбрасываются)");
+    Console.WriteLine("6 - вычесть целое число из рублей");
+    Console.WriteLine("7 - вычесть объект из числа");
+    Console.WriteLine("8 - выход");
+    int number_problem = IntInputValidator.GetValidIntInput();
+    if (number_problem == 1)
+    {
+        Money money3 = money2 - money1;
+        Console.WriteLine(money3.ToString());
+        Console.WriteLine();
+    }
+    else if (number_problem == 2)
+    {
+        Console.WriteLine(money1.ToString());
+        money1++;
+        Console.WriteLine(money1.ToString());
+        Console.WriteLine();
+    }
+    else if (number_problem == 3)
+    {
+        Console.WriteLine(money1.ToString());
+        money1--;
+        Console.WriteLine(money1.ToString());
+        Console.WriteLine();
+    }
+    else if (number_problem == 4)
+    {
+        uint x = money1;
+        Console.WriteLine(x);
+        Console.WriteLine();
+    }
+    else if (number_problem == 5)
+    {
+        double y = (double)money1;
+        Console.WriteLine(y);
+        Console.WriteLine();
+    }
+    else if (number_problem == 6)
+    {
+        uint x = (uint)(IntInputValidator.GetValidIntInput());
+        Money z = money1 - x;
+        Console.WriteLine(z.ToString());
+        Console.WriteLine();
+    }
+    else if (number_problem == 7)
+    {
+        uint x = (uint)(IntInputValidator.GetValidIntInput());
+        Money z = x - money1;
+        Console.WriteLine(z.ToString());
+        Console.WriteLine();
+    }
+    else if (number_problem == 8)
+    {
+        tr1 = false;
+    }
+}
